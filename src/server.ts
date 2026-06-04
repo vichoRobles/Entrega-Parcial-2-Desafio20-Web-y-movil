bb2bc79735bcb7bcd7e82e8272ca159a7dd3dcfc
pancakes2 <41842151+pancakes2@users.noreply.github.com>
2026-06-03 22:27:32 -0400

    Agregadas las rutas proyecto, opinion, usuario y auth para la autentificación.

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import dotenv from 'dotenv';
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { checkDatabaseConnection } from './config/db';
+import proyectoRoutes from './routes/proyecto.routes';
+import opinionRoutes from './routes/opinion.routes';
+import authRoutes from './routes/auth.routes';
+import usuarioRoutes from './routes/usuario.routes';
 
 // Variables de entorno
 dotenv.config();
@@ -17,7 +21,10 @@ app.get('/api/health', (req: Request, res: Response) => {
   res.status(200).json({ status: 'success', message: 'API corriendo correctamente!' });
 });
 
-
+app.use('/api/proyectos', proyectoRoutes);
+app.use('/api/opiniones', opinionRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/usuarios', usuarioRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server corriendo en el puerto ${PORT}`);
